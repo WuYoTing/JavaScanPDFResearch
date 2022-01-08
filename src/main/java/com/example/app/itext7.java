b5d9f1cd84b6ef105b82fda8d6d8f1467de2d72b
Tim <hakosaki314@gmail.com>
2022-01-08 18:03:23 +0800

    refactor: 修改 itext7 實作 可抓取字串文字

--- src/main/java/com/example/app/itext7.java
+++ src/main/java/com/example/app/itext7.java
@@ -1,39 +1,56 @@
 package com.example.app;
 
-import com.itextpdf.kernel.pdf.PdfArray;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.IPdfTextLocation;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.RegexBasedLocationExtractionStrategy;
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
-import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
-import com.itextpdf.kernel.pdf.canvas.parser.listener.LocationTextExtractionStrategy;
-import com.itextpdf.forms.PdfAcroForm;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class itext7 {
 
-  public static final String DEST = "C:\\Users\\yung\\Desktop\\記憶體發票.txt";
-
-  public static final String SRC = "C:\\Users\\yung\\Desktop\\文件\\記憶體發票.pdf";
+  public static final String SRC = "";
 
   public static void main(String[] args) throws IOException {
-    File file = new File(DEST);
-    file.getParentFile().mkdirs();
-
-    new itext7().manipulatePdf(DEST);
+    String a = manipulatePdf();
   }
 
-  protected void manipulatePdf(String dest) throws IOException {
+  private static String manipulatePdf() throws IOException {
     PdfDocument pdfDoc = new PdfDocument(new PdfReader(SRC));
-    // Create a text extraction renderer
-    LocationTextExtractionStrategy strategy = new LocationTextExtractionStrategy();
-    // Note: if you want to re-use the PdfCanvasProcessor, you must call PdfCanvasProcessor.reset()
-    PdfCanvasProcessor parser = new PdfCanvasProcessor(strategy);
-    parser.processPageContent(pdfDoc.getFirstPage());
-    String actualText = strategy.getResultantText();
-    System.out.println(actualText);
-    pdfDoc.close();
 
+    System.out.println(pdfDoc.getNumberOfPages());
+    for (int i = 1; i <= pdfDoc.getNumberOfPages(); i++) {
+      PdfPage page = pdfDoc.getPage(i);
+      // new Regex Based Strategy with .*
+      RegexBasedLocationExtractionStrategy strategy = new RegexBasedLocationExtractionStrategy(".*");
+      // New Stream Processor that will send its output to the designated render listener
+      PdfCanvasProcessor canvasProcessor = new PdfCanvasProcessor(strategy);
+      // Creates a new PDF Content Stream Processor that will send its output to the designated render listener.
+      canvasProcessor.processPageContent(page);
+      // Returns the Rectangles that have been processed so far
+      Collection<IPdfTextLocation> resultantLocations = strategy.getResultantLocations();
+
+      System.out.println("---------------------------");
+      for (IPdfTextLocation location : resultantLocations) {
+        Rectangle rectangle = location.getRectangle();
+        System.out.println(location.getPageNumber());
+        System.out.println(location.getText());
+        System.out.printf("X : %f \n", rectangle.getX());
+        System.out.printf("Y : %f \n", rectangle.getY());
+        System.out.printf("Width : %f \n", rectangle.getWidth());
+        System.out.printf("Height : %f \n", rectangle.getHeight());
+        System.out.println("---------------------------");
+      }
+    }
+    pdfDoc.close();
+    return "hello";
   }
 }
